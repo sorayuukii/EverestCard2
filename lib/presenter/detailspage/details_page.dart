f28be30149853e4c35590d1def5ac7c1a5015e4a
William Fernandes~ <william.fernandes@warren.com.br>
2022-09-15 10:00:48 -0300

    feat: using fl_chat

--- lib/presenter/detailspage/details_page.dart
+++ lib/presenter/detailspage/details_page.dart
@@ -1,18 +1,39 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class DetailsPage extends StatelessWidget {
   const DetailsPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: const Center(
-        child: Text('Next Page'),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => LineChart(
+        LineChartData(
+          titlesData: FlTitlesData(
+            show: false,
+          ),
+          minX: 0,
+          maxX: 3,
+          minY: 0,
+          maxY: 3,
+          gridData: FlGridData(
+            show: true,
+            drawVerticalLine: false,
+            drawHorizontalLine: false,
+          ),
+          lineBarsData: [
+            LineChartBarData(
+              spots: const [
+                FlSpot(0, 1),
+                FlSpot(1, 2),
+                FlSpot(2, 2.2),
+                FlSpot(2.2, 2.5),
+                FlSpot(2.5, 2.6),
+                FlSpot(2.6, 2.7),
+                FlSpot(2.7, 2.8),
+                FlSpot(2.8, 2.9),
+                FlSpot(2.9, 3.0),
+              ],
+            ),
+          ],
+        ),
+      );
 }
